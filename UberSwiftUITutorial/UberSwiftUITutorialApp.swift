4da3c9b55fe2b229344f3c5100d6c0ac58929bf3
Ivan Verdugo <hedgarvy@gmail.com>
2026-05-22 11:10:18 -0700

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Ivan Verdugo on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
